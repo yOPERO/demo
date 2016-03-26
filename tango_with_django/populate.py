00182f538ec351b02106470e176f6dd8849f18e6
yOPERO <yopero666@gmail.com>
2016-03-26 20:21:59 +0000

    add PageAdmin view in the admin iface

--- tango_with_django/populate.py
+++ tango_with_django/populate.py
@@ -10,7 +10,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', 128, 64)
 
     add_page(cat=python_cat,
              title="Official Python Tutorial",
@@ -24,7 +24,7 @@ def populate():
              title="Learn Python in 10 Minutes",
              url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
              title="Official Django Tutorial",
@@ -38,7 +38,7 @@ def populate():
              title="How to Tango with Django",
              url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", 32, 16)
 
     add_page(cat=frame_cat,
              title="Bottle",
@@ -62,8 +62,11 @@ def add_page(cat, title, url, views=0):
     return p
 
 
-def add_cat(name):
+def add_cat(name, view=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.view = view
+    c.likes = likes
+    c.save()
     return c
 
 
